abc3591a52459bdc3b4e7a461bf168b033760090
wjdduf4439 <wjdduf4439@gmail.com>
2023-06-16 05:14:49 +0900

    20230616 build

---
+++ src/main/java/com/ljy/third/controller/homeController.java
@@ -0,0 +1,14 @@
+package com.ljy.third.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class homeController {
+
+	@GetMapping("/helloWorld")
+	public String test() {
+		return "Do you want to build a Snowman?";
+	}
+	
+}
