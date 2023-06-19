088afb2cf93aa701dabb08cf7c0df0737a579c02
wjdduf4439 <wjdduf4439@gmail.com>
2023-06-20 02:27:43 +0900

    20230620build

--- src/main/java/com/ljy/third/controller/homeController.java
+++ src/main/java/com/ljy/third/controller/homeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class homeController {
 
-	@GetMapping("/helloWorld")
+	@GetMapping("/")
 	public String test() {
 		return "Do you want to build a Snowman?";
 	}
